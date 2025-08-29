132d5a2311e49c8bd16b2f4d702277c36f7814d5
jay <jaygoswami600@gmail.com>
2025-08-29 10:43:27 +0530

    changed calc file

--- calc.js
+++ calc.js
@@ -1,15 +1,17 @@
-module.exports.add = function(x,y){
+module.exports.add = function (x, y) {
     return x + y;
-}
+};
+
+// some comment
 
 
-module.exports.sub = function (x,y){
-    return x -y;
-}
+module.exports.sub = function (x, y) {
+    return x - y;
+};
 
-module.exports.mul = function (x,y){
-    return x *y;
-}
+module.exports.mul = function (x, y) {
+    return x * y;
+};
 
 
 // module.exports = sub;
